4a555c6eb816118ec5be0290f38869d4ace3d7a1
Jessica Medeiros <jessicamedeirosp96@gmail.com>
2024-08-23 17:18:19 -0300

    feat: file boostrap

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello, world!");
+import { Analyzer } from "./application/Analyzer";
+import { CommandLine } from "./application/CommandLine";
+
+async function bootstrap() {
+  const analyzer = new Analyzer();
+  const commandLine = new CommandLine();
+  // const  depth = cliParser.getDepth();
+  const verbose = commandLine.isVerbose();
+  const phrase = commandLine.getPhrase();
+
+  if (!phrase) {
+    console.log("Adicione uma frase");
+    return;
+  }
+
+  const result = analyzer.analyzePhrase(phrase);
+  console.log(result);
+
+  if (verbose) {
+    console.log("carregamento dos parametros");
+    console.log("Tempo de verificacao da frase");
+  }
+}
+
+bootstrap();
