4cb4f5324629891feb8f9e944f4e6d7cadc94434
Jessica Medeiros <jessicamedeirosp96@gmail.com>
2024-08-23 18:46:49 -0300

    feat: adding runtime metrics

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,37 @@ import { Analyzer } from "./application/Analyzer";
 import { CommandLine } from "./application/CommandLine";
 
 async function bootstrap() {
+  const startTimeParams = process.hrtime();
   const analyzer = new Analyzer();
   const commandLine = new CommandLine();
   // const  depth = cliParser.getDepth();
   const verbose = commandLine.isVerbose();
   const phrase = commandLine.getPhrase();
 
+  const endTimeParams = process.hrtime(startTimeParams);
+  const timeTakenParams = endTimeParams[0] * 1000 + endTimeParams[1] / 1000000;
+
   if (!phrase) {
     console.log("Adicione uma frase");
     return;
   }
 
+  const startTimeAnalysis = process.hrtime();
+
   const result = analyzer.analyzePhrase(phrase);
   console.log(result);
 
+  const endTimeAnalysis = process.hrtime(startTimeAnalysis);
+  const timeTakenAnalysis =
+    endTimeAnalysis[0] * 1000 + endTimeAnalysis[1] / 1000000;
+
   if (verbose) {
-    console.log("carregamento dos parametros");
-    console.log("Tempo de verificacao da frase");
+    console.log(
+      `Tempo de carregamento dos parâmetros: ${timeTakenParams.toFixed(3)} ms`,
+    );
+    console.log(
+      `Tempo de verificação da frase: ${timeTakenAnalysis.toFixed(3)} ms`,
+    );
   }
 }
 
