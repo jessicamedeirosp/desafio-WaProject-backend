d2e5592f5054aff500d349fbe945422b071a4eba
Jessica Medeiros <jessicamedeirosp96@gmail.com>
2024-08-23 17:14:49 -0300

    refactor: adding normalizeWord

--- src/domain/Hierarchy.ts
+++ src/domain/Hierarchy.ts
@@ -2,14 +2,20 @@ import { IAnimal } from "./Animal";
 
 export class Hierarchy {
   findByName(name: string, item: IAnimal): IAnimal | null {
-    if (item.name === name) return item;
+    if (this.normalizeWord(item.name) === this.normalizeWord(name)) return item;
 
     for (const child of item.children) {
       const result = this.findByName(name, child);
-      if (result) 
-        return result;
+      if (result) return result;
     }
 
     return null;
+  }
+
+  normalizeWord(word: string) {
+    return word
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "");
   }
 }
