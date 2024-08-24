c400dc6592d7c986628094f53297ce2e67b96627
Jessica Medeiros <jessicamedeirosp96@gmail.com>
2024-08-24 16:47:51 -0300

    test: commandLine

--- test/CommandLine.spec.ts
+++ test/CommandLine.spec.ts
@@ -1,5 +1,38 @@
+import { CommandLine } from "../src/application/CommandLine";
+
+jest.mock("yargs/helpers", () => ({
+  hideBin: jest.fn().mockImplementation((args) => args),
+}));
+
+jest.mock("yargs", () => {
+  const mockYargs = {
+    command: jest.fn().mockReturnThis(),
+    help: jest.fn().mockReturnThis(),
+    parseSync: jest.fn().mockReturnValue({
+      depth: 2,
+      verbose: true,
+      phrase: "Eu amo papagaios",
+    }),
+  };
+  return jest.fn(() => mockYargs);
+});
+
 describe("CommandLine", () => {
-  test("for test", () => {
-    expect(2 + 2).toBe(4);
+  let commandLine: CommandLine;
+
+  beforeEach(() => {
+    commandLine = new CommandLine();
+  });
+
+  test("getDepth returns correct depth", () => {
+    expect(commandLine.getDepth()).toBe(2);
+  });
+
+  test("isVerbose returns correct verbose flag", () => {
+    expect(commandLine.isVerbose()).toBe(true);
+  });
+
+  test("getPhrase returns correct phrase", () => {
+    expect(commandLine.getPhrase()).toBe("Eu amo papagaios");
   });
 });
